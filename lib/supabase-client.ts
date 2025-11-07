bb4e1eb5d4ec45c515ac9983fdd320eaf21451a2
rase51 <cerober1903@gmail.com>
2025-11-07 01:18:11 -0500

    Ahora las keys usan NEXT_PUBLIC_ y env vars

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const SUPABASE_URL = "https://zgdpukvvhxggynlaqygh.supabase.co"
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnZHB1a3Z2aHhnZ3lubGFxeWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzOTExNjEsImV4cCI6MjA3Nzk2NzE2MX0.d-vCVy0o1dnslRXHsg7QDc2EOSLrVP6XWc4Smh5EB9k"
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
